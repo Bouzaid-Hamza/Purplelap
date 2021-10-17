61ef16c17025e41d0683e657b5ebf4583c6cef83
Bouzaid-Hamza <hmzabd3@gmail.com>
2021-10-17 21:35:58 +0200

    commit

--- server/startup/production.js
+++ server/startup/production.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const path = require('path');
-const helmet = require('helmet');
+// const helmet = require('helmet');
 const compression = require('compression');
 const filePath = path.join(__dirname, '../../client/build');
 
 module.exports = (app) => {
-    app.use(helmet());
+    // app.use(helmet({
+    //     contentSecurityPolicy: {
+    //         directives: {
+    //             ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+    //             'script-src': ["'self'", "'unsafe-inline'", 'example.com']
+    //         }
+    //     }
+    // }));
     app.use(compression());
 
     app.use(express.static(filePath));
